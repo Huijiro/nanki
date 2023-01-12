7e8797d83ea37e2e4ae04b36a8e011a61f332137
huijiro <huijirohankei@gmail.com>
2023-01-12 18:32:49 -0300

    feat(home): Added categories to main page.

---
+++ src/components/Divs/TitleBox.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let title = 'DEFAULT';
+</script>
+
+<div class="p-5 w-full h-full">
+	<h1 class="font-bold text-xl border-b-2">{title}</h1>
+	<div class="p-3">
+		<slot />
+	</div>
+</div>
