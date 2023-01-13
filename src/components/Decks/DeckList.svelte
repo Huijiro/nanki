6b2670a5fcfc7105195516fb815f9080a7bd42c0
huijiro <huijirohankei@gmail.com>
2023-01-13 19:00:04 -0300

    feat(deck): Create new deck button.

--- src/components/Decks/DeckList.svelte
+++ src/components/Decks/DeckList.svelte
@@ -1,9 +1,11 @@
 <script>
 	import DeckDisplay from './DeckDisplay.svelte';
+	import DeckNew from './DeckNew.svelte';
 </script>
 
 <div class="flex flex-wrap gap-3">
-	{#each Array(100) as _}
+	{#each Array(10) as _}
 		<DeckDisplay />
 	{/each}
+	<DeckNew />
 </div>
